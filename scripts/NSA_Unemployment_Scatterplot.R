5f26ac2923dd8d2caa4420912b0a011cb0d606d9
Graham Fulton <grahamfulton@Grahams-MacBook-Air.local>
2020-11-11 09:32:18 -0800

    Fix dates in plot

--- scripts/NSA_Unemployment_Scatterplot.R
+++ scripts/NSA_Unemployment_Scatterplot.R
@@ -2,15 +2,15 @@
 library("tidyverse")
 
 #Load dataset
-unemployment <- read_csv(file = "DoL_Unemployment.csv")
+unemployment <- read_csv("datasets/DoL_Unemployment.csv")
 
 #Take Non Seasonal Adjusted (NSA) Initial Unemployment claims and date since
 #January 4th, 2020 and rename
 non_seasonal_adjusted <- unemployment %>%
   rename(Date = X1) %>%
-  summarise(Date, N.S.A)
+  mutate(Date = as.Date(Date, format = "%m/%d/%Y")) %>% 
+  select(Date, N.S.A)
 
-#Build a scatterplot, but without connecting line
 unemployment_plot <- ggplot(data = non_seasonal_adjusted,
                             aes(x = Date, y = N.S.A, group = 1)) +
   geom_line() +
