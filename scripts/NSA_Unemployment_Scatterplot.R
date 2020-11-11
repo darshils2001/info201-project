8ae5abadeda8022289c7c952648d0a0405ed87cc
Graham Fulton <grahamfulton@Grahams-MacBook-Air.local>
2020-11-11 09:52:12 -0800

    Update comments

--- scripts/NSA_Unemployment_Scatterplot.R
+++ scripts/NSA_Unemployment_Scatterplot.R
@@ -4,6 +4,9 @@ library("tidyverse")
 #Load dataset
 unemployment <- read_csv("datasets/DoL_Unemployment.csv")
 
+#Load COVID-19 Cases dataset
+national <- read.csv("https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv")
+
 #Take Non Seasonal Adjusted (NSA) Initial Unemployment claims and date since
 #January 4th, 2020 and rename
 non_seasonal_adjusted <- unemployment %>%
@@ -11,6 +14,7 @@ non_seasonal_adjusted <- unemployment %>%
   mutate(Date = as.Date(Date, format = "%m/%d/%Y")) %>% 
   select(Date, N.S.A)
 
+#Build scatterplot of unemployment claims per week
 unemployment_plot <- ggplot(data = non_seasonal_adjusted,
                             aes(x = Date, y = N.S.A, group = 1)) +
   geom_line() +
@@ -18,5 +22,3 @@ unemployment_plot <- ggplot(data = non_seasonal_adjusted,
   labs(title = "Initial Unemployment Claims Per Week",
        x = "Date",
        y = "NSA Unemployment Claims")
-
-unemployment_plot
