a651ab9cfffd1f30353eae4374c68de59e046c59
Darshil Shah <36972821+darshils2001@users.noreply.github.com>
2020-11-11 09:16:18 -0800

    Change to relative path

--- scripts/Sector_Bar_Chart.R
+++ scripts/Sector_Bar_Chart.R
@@ -4,7 +4,7 @@ library("ggplot2")
 library("tidyverse")
 
 # Load Dataset
-sectors <- read_excel("/Users/Darshil/Documents/INFO201/info201-project/datasets/National_Sector_Dataset.xls")
+sectors <- read_excel("datasets/National_Sector_Dataset.xls")
 
 # Convert Sector Numbers to Names
 sectors$NAICS_SECTOR <- gsub(11, "Agriculture", sectors$NAICS_SECTOR)
